c1dd75642e1feba8dcb1bf29a18a4a6897ace564
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-14 21:04:44 +0200

    chore - feature text updated

--- app/components/about/Features.tsx
+++ app/components/about/Features.tsx
@@ -3,18 +3,16 @@ import { Feature } from "./Feature";
 export const Features = () => {
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-2 gap-10">
-    
       <Feature
         title="Recorded sessions"
         imageUrl="/hero.jpg"
-        description="
-                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex neque voluptatibus quo sint non rem debitis dolor voluptas labore placeat suscipit quisquam pariatur, quae, cum similique enim possimus! Iure iusto ab ad fugiat. Voluptates delectus debitis quis accusamus cum id ab dolorum deserunt facere autem beatae repellat, expedita, maxime tempora!"
+        description="Embark on a comprehensive exploration of user engagement with our recorded sessions. Immerse yourself in a visual journey that unveils the intricacies of your audience's interaction on your website. Gain profound insights into user behavior and preferences, empowering you to optimize and enhance the overall user experience."
       />
       <Feature
         title="Insights"
         imageUrl="/hero.jpg"
         description="
-                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex neque voluptatibus quo sint non rem debitis dolor voluptas labore placeat suscipit quisquam pariatur, quae, cum similique enim possimus! Iure iusto ab ad fugiat. Voluptates delectus debitis quis accusamus cum id ab dolorum deserunt facere autem beatae repellat, expedita, maxime tempora!"
+        Revolutionize your decision-making process with our robust insights. Going beyond mere data, our analytics provide a transformative understanding of your website's performance. From discerning user trends to formulating impactful strategies, immerse yourself in the knowledge needed to elevate your online presence and drive sustained success."
       />
     </div>
   );
