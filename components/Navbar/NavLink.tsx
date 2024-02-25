29dce02656ec44c5f8ed398564be65c27c518665
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-25 13:13:37 +0200

    chore - nav link ui

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { Circle } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FC } from "react";
 
 interface LinkProps {
@@ -8,12 +11,22 @@ interface LinkProps {
 }
 
 export const NavLink: FC<LinkProps> = ({ label, where }) => {
+  const pathname = usePathname();
+
   return (
     <Link
       href={where}
-      className="flex items-center gap-2 border-b-2 border-white hover:border-purple transition-colors group"
+      className={cn(
+        "flex items-center gap-2 border-b-2 border-white hover:border-purple transition-colors group",
+        pathname.includes(where) && "border-b-2 border-purple"
+      )}
     >
-      <Circle className="h-2 w-2 group-hover:text-purple transition-colors" />
+      <Circle
+        className={cn(
+          "h-2 w-2 group-hover:text-purple transition-colors",
+          pathname.includes(where) && "text-purple"
+        )}
+      />
       <span className="text-sm font-medium">{label}</span>
     </Link>
   );
