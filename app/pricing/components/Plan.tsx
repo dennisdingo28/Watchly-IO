10e82e6a8b138ebdd90c9878aa4929d35aa53b3e
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-25 18:32:37 +0200

    chore - pricing page ui

--- app/pricing/components/Plan.tsx
+++ app/pricing/components/Plan.tsx
@@ -1,27 +1,35 @@
-import { Plan as PlanType } from "@/types"
-import { PlanBenefit } from "./PlanBenefit"
-import { Badge } from "@/components/ui/badge"
-import { PurchaseButton } from "./PurchaseButton"
+import { Plan as PlanType } from "@/types";
+import { PlanBenefit } from "./PlanBenefit";
+import { Badge } from "@/components/ui/badge";
+import { PurchaseButton } from "./PurchaseButton";
 
-export const Plan = ({plan}:{plan: PlanType}) =>{
-    return (
-        <div className="relative w-full bg-white cursor-pointer rounded-md p-2 shadow-[0px_0px_4px_rgb(119,109,242)] hover:shadow-[0px_0px_10px_rgb(119,109,242)] duration-150 hover:scale-105">
-            {plan.badge ? <Badge className="absolute top-1 right-1 bg-darkPurple">{plan.badge}</Badge> : null}
-            <p className="font-semibold text-2xl">{plan.name}</p>
-            <p className="text-sm text-darkGray">{plan.description}</p>
+export const Plan = ({ plan }: { plan: PlanType }) => {
+  return (
+    <div className="relative w-full bg-white cursor-pointer rounded-md p-2.5 shadow-[0px_0px_4px_rgb(119,109,242)] hover:shadow-[0px_0px_10px_rgb(119,109,242)] hover:scale-105 transition-transform">
+      {plan.badge ? (
+        <Badge className="absolute top-1 right-1 bg-darkPurple">
+          {plan.badge}
+        </Badge>
+      ) : null}
+      <p className="font-semibold text-2xl">{plan.name}</p>
+      <p className="text-sm text-darkGray">{plan.description}</p>
 
-            <div className="mt-4 space-y-2">
-                {plan.benefits.map((benefit, idx)=>(
-                    <PlanBenefit key={idx} benefit={benefit}/>
-                ))}
-            </div>
+      <div className="mt-5 space-y-2.5">
+        {plan.benefits.map((benefit, idx) => (
+          <PlanBenefit key={idx} benefit={benefit} />
+        ))}
+      </div>
 
-            <div className="my-5">
-                <p className="flex items-center gap-1"> <span className="font-bold text-3xl">${plan.newPrice}</span> <span className="text-sm"> /month</span></p>
-            </div>
-            <div className="flex justify-center items-end">
-                <PurchaseButton/>
-            </div>
-        </div>
-    )
-}
+      <div className="my-5">
+        <p className="flex items-center gap-1">
+          {" "}
+          <span className="font-bold text-3xl">${plan.newPrice}</span>{" "}
+          <span className="text-sm"> /month</span>
+        </p>
+      </div>
+      <div className="flex justify-center items-end">
+        <PurchaseButton />
+      </div>
+    </div>
+  );
+};
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -15,18 +15,18 @@ const PricingPage = () => {
         <h1 className="text-4xl font-semibold text-center">
           <InfoText>Limited offers!</InfoText>
         </h1>
-        <p className="text-darkGray mt-4 text-sm text-center max-w-[600px] mx-auto">
+        <p className="text-darkGray mt-5 text-sm text-center max-w-[600px] mx-auto">
           Find the perfect plan for your goals. Explore pricing options that
           offer unmatched value, tailored to meet your business needs.
         </p>
       </section>
-      <section className="mt-10 flex flex-col md:flex-row justify-center gap-6">
+      <section className="mt-10 flex flex-col md:flex-row justify-center gap-5">
         {plans.map((plan, idx) => (
           <Plan key={idx} plan={plan} />
         ))}
       </section>
       <section className="mt-10">
-        <Faq/>
+        <Faq />
       </section>
     </Container>
   );
