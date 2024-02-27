04aefbba22a375cc8700dd70e6dfe7d08c199767
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-27 13:16:21 +0200

    chore same query

--- app/dashboard/components/WorkspaceFeed.tsx
+++ app/dashboard/components/WorkspaceFeed.tsx
@@ -1,33 +1,34 @@
 "use client";
 import React from "react";
 import { Workspace as WorkspaceType } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";;
 import { Workspace } from "./Workspace";
 import { getWorkspaces } from "@/lib/queryFns/getWorkspaces";
+
 
 export const WorkspaceFeed = ({
   initialWorkspaces,
 }: {
   initialWorkspaces: WorkspaceType[];
 }) => {
+
   const { data } = useQuery({
     queryKey: ["workspaces"],
     queryFn: getWorkspaces,
+
     initialData: initialWorkspaces,
-    staleTime: 5 * 1000,
   });
+
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-      {Array(
-        data?.map((workspace) => (
-          <Workspace
-            key={workspace.id}
-            name={workspace.name}
-            createdAt={workspace.createdAt}
-          />
-        ))
-      )}
+      {Array(data?.map((workspace) => (
+        <Workspace
+          key={workspace.id}
+          name={workspace.name}
+          createdAt={workspace.createdAt}
+        />
+      )))}
     </div>
   );
 };
