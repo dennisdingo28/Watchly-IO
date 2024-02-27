1537b9cfab187726bbf9d21c6bc80405a090b38d
Moldovan Dennis <dennismoldovan32@gmail.com>
2024-02-27 12:03:17 +0200

    chore - .env URL

--- lib/queryFns/getWorkspaces.ts
+++ lib/queryFns/getWorkspaces.ts
@@ -1,8 +1,9 @@
 import { Workspace } from "@prisma/client";
 
 export const getWorkspaces = async () => {
-  const res = await fetch("http://localhost:3000/api/queryFns/workspaces");
+  const res = await fetch(`${process.env.APP_URL}/api/queryFns/workspaces`);
       const json = await res.json();
+      console.log("josn", json);
       
       return json as Workspace[];
 };
