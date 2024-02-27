d8a54c6390561b3c67a2169a39ddcf4c57cc5e6e
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-02-27 20:05:17 +0200

    chore - test route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,12 +1,15 @@
 import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest){
     try{
-        const user = await currentUser();
-        if (!user) return new NextResponse("Unauthorized", { status: 401 });
+        // const user = await currentUser();
+        // if (!user) return new NextResponse("Unauthorized", { status: 401 });
 
-        return NextResponse.json([{title:"ding", description:"asfsa"}]);
+        const users = await db.user.findMany({})
+
+        return NextResponse.json(users);
     }catch(err){
         console.log(err);
         return new NextResponse("Internal Error");
