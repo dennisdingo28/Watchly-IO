1c69de0b5da306e3134e197ffc99e24183134d9d
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-02-27 13:23:32 +0200

    chore - add auth

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,10 @@
+import { currentUser } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest){
     try{
+        const user = await currentUser();
+        if (!user) return new NextResponse("Unauthorized", { status: 401 });
         return NextResponse.json([{title:"ding", description:"asfsa"}]);
     }catch(err){
         console.log(err);
