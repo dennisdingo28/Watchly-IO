c7671713e9484c5fb605568aed7029cea3671b46
Moldovan Dennis <dennismoldovan32@gmail.com>
2024-02-27 11:51:36 +0200

    chore - change url

--- lib/queryFns/getWorkspaces.ts
+++ lib/queryFns/getWorkspaces.ts
@@ -1,7 +1,7 @@
 import { Workspace } from "@prisma/client";
 
 export const getWorkspaces = async () => {
-  const res = await fetch(`https://watchly-io.vercel.app/api/queryFns/workspaces`, {next:{revalidate:0}});
+  const res = await fetch("https://watchly-io.vercel.app/api/queryFns/workspaces");
       const json = await res.json();
       
       return json as Workspace[];
