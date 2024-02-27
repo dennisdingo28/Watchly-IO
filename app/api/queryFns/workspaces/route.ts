22438e674cc31893120f19f0f65922fc65c87e7f
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-02-27 20:45:35 +0200

    chore - change id

--- app/api/queryFns/workspaces/route.ts
+++ app/api/queryFns/workspaces/route.ts
@@ -1,15 +1,16 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { unstable_noStore as noStore } from 'next/cache';
+import { currentUser } from "@/lib/auth";
 
 export async function GET(req: Request) {
   noStore();
 
   try {
-
+    
     const workspaces = await db.workspace.findMany({
       where: {
-        userId:"clt4p03gq0000mrrs1kwhhrqu",
+        userId:"clt4pwp150000qt9jyuyo5q20",
       },
     });
     
