662aa74b0f597ef25e63dc195195ca9040ac3830
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-29 10:13:40 +0200

    chore - dashboard id ui fixes

--- app/dashboard/[id]/components/overall/OverallData.tsx
+++ app/dashboard/[id]/components/overall/OverallData.tsx
@@ -1,11 +1,10 @@
 import { Flag, User2 } from "lucide-react";
 import { OverallCard } from "./OverallCard";
 
-
 export const OverallData = () => {
   return (
     <>
-      <div className="grid grid-cols-2 md:items-center justify-between gap-y-6 xsBig:grid-cols-4 xsBig:gap-10">
+      <div className="grid grid-cols-2 md:items-center justify-between gap-y-5 xsBig:grid-cols-4 xsBig:gap-10">
         <OverallCard
           icon={<User2 className="text-purple w-5 h-5" />}
           amount={2546}
--- app/dashboard/[id]/components/project/ProjectManager.tsx
+++ app/dashboard/[id]/components/project/ProjectManager.tsx
@@ -2,14 +2,14 @@ import { ProjectApiKey } from "./ProjectApiKey";
 
 export const ProjectManager = () => {
   return (
-    <div className="cursor-pointer bg-gray-100 px-4 py-2 rounded-md duration-100 hover:bg-transparent hover:shadow-[0px_0px_3px_rgb(119,109,242)]">
+    <div className="cursor-pointer bg-lightGray px-5 py-2.5 rounded-md hover:bg-transparent hover:shadow-[0px_0px_3px_rgb(119,109,242)] transition-colors">
       <div className="flex items-center gap-2">
         <p className="font-bold">Dennis&apos;s Workspace</p>
         <div className="w-[10px] h-[10px] rounded-full bg-green-500 animate-pulse" />
       </div>
       <div>
         <p className="text-sm text-darkGray">Plan: Business</p>
-        <ProjectApiKey apiKey="watchly-asfa81251@gasa2"/>
+        <ProjectApiKey apiKey="watchly-asfa81251@gasa2" />
       </div>
     </div>
   );
