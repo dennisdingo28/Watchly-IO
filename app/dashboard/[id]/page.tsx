d3b2c755d16ba3f8d4b06b9e9f9c52dde8bb0a60
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-03-01 21:30:37 +0200

    chore - socket init adjustments

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,7 +1,23 @@
+import { db } from "@/lib/db";
+import { InitSocket } from "./components/InitSocket";
 import { WorkspaceData } from "./components/workspace/WorkspaceData";
+import { redirect } from "next/navigation";
 
-const DashboardProjectPage = () => {
-  return <WorkspaceData />;
+const DashboardProjectPage = async ({params}:{params:{id: string}}) => {
+  const workspace = await db.workspace.findUnique({
+    where:{
+      id: params.id,
+    },
+  });
+
+  if(!workspace) redirect("/dashboard");
+
+  return (
+    <>
+    <WorkspaceData/>
+    <InitSocket roomId={workspace.roomId}/>
+    </>
+  )
 };
 
 export default DashboardProjectPage;
