1e57bea37f49bdc97e4e0c6a9dcf4b8c825247a2
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-02 20:42:39 +0200

    chore - recording ui

--- app/dashboard/[id]/components/recordings/EditRecording.tsx
+++ app/dashboard/[id]/components/recordings/EditRecording.tsx
@@ -25,10 +25,12 @@ import {
   EditRecordingSchemaType,
 } from "@/validators/editRecording";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Trash2 } from "lucide-react";
+import { PlayCircle, Trash2 } from "lucide-react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { HiDotsHorizontal } from "react-icons/hi";
+import { Recording } from "./Recording";
+import Image from "next/image";
 
 interface EditRecordingProps {
   userName: string;
@@ -62,9 +64,26 @@ export const EditRecording = ({
               </DrawerDescription>
             </div>
             <OpenModal type="deleteRecording">
-              <Trash2 className="text-rose-500 w-[18px] h-[18px]" />
+              <Trash2 className="text-rose-500 w-[18px] h-[18px] cursor-pointer" />
             </OpenModal>
           </DrawerHeader>
+          <div className="flex flex-col mb-5">
+            <div className="relative">
+              <Image
+                src="/about1.jpg"
+                width={300}
+                height={300}
+                alt="video-thumbnail"
+                className="border-2 object-cover w-full max-h-[300px]"
+              />
+              <div className="absolute bg-black bg-opacity-65 inset-0 flex items-center justify-center">
+                <div className="space-y-2.5 text-darkGray">
+                  <PlayCircle className="w-[64px] h-[64px]  mx-auto" />
+                  <span className="text-sm text-center">12:05 minutes</span>
+                </div>
+              </div>
+            </div>
+          </div>
           <Form {...form}>
             <form className="mb-2.5">
               <FormField
