8094f734dac1d9a88bf0485ce8cf5bd2e9214c96
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-02 20:47:08 +0200

    chore - workspace manager ui fixes

--- app/dashboard/[id]/components/workspace/WorkspaceManager.tsx
+++ app/dashboard/[id]/components/workspace/WorkspaceManager.tsx
@@ -2,7 +2,7 @@ import { WorkspaceApiKey } from "./WorkspaceApiKey";
 
 export const WorkspaceManager = () => {
   return (
-    <div className="cursor-pointer bg-lightGray px-5 py-2.5 rounded-md hover:bg-transparent hover:shadow-[0px_0px_3px_rgb(119,109,242)] transition-colors">
+    <div className="cursor-pointer bg-lightGray px-5 py-2.5 rounded-md hover:bg-transparent hover:shadow-[0px_0px_3px_rgb(119,109,242)] transition-colors w-fit">
       <div className="flex items-center gap-5">
         <p className="font-bold">Dennis&apos;s Workspace</p>
         <div className="w-[10px] h-[10px] rounded-full bg-green-500 animate-pulse" />
