32267c71167a185a7f536952ff93349e24a9b53c
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-03 19:45:36 +0200

    chore - workspace name ui

--- app/dashboard/[id]/components/workspace/WorkspaceName.tsx
+++ app/dashboard/[id]/components/workspace/WorkspaceName.tsx
@@ -1,79 +1,96 @@
-"use client"
+"use client";
 
 import axios from "axios";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { WorkspaceRequest, WorkspaceValidator } from "@/validators/workspace";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { useEffect, useState } from "react"
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useClickOutside } from "@mantine/hooks";
-import { revalidatePathname } from "@/lib/revalidatePathname";
 import { WorkspaceWithUsers } from "@/types";
+import { getWorkspaceName } from "@/lib/queryFns/getWorkspaceName";
 
+export const WorkspaceName = ({
+  workspace,
+}: {
+  workspace: WorkspaceWithUsers;
+}) => {
+  const { data } = useQuery({
+    queryKey: ["workspaceName"],
+    queryFn: () => getWorkspaceName(workspace.id),
+    initialData: workspace,
+  });
 
-export const WorkspaceName = ({workspace}:{workspace: WorkspaceWithUsers}) =>{
-  
-   
-    const [isEditing, setIsEditing] = useState(false);
-    const [showErrors, setShowErrors] = useState(false);
-    
-    const {handleSubmit, register, formState:{errors}, getValues} = useForm<WorkspaceRequest>({
-        resolver: zodResolver(WorkspaceValidator),
-        defaultValues:{
-            name: workspace.name,
-        },
-    });
+  const [isEditing, setIsEditing] = useState(false);
+  const [showErrors, setShowErrors] = useState(false);
 
-    const editRef =  useClickOutside(()=>{
-        if(getValues("name")!==workspace.name){
-            updateWorkspaceName(getValues("name"));
-        }
-        else setIsEditing(false);
-    });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    getValues,
+  } = useForm<WorkspaceRequest>({
+    resolver: zodResolver(WorkspaceValidator),
+    defaultValues: {
+      name: data.name,
+    },
+  });
 
-    const {mutate: updateWorkspaceName, isPending} = useMutation({
-        mutationFn: async(newName: string)=>{
-            const res = await axios.patch(`/api/workspace/${workspace.id}`, {name: newName});
+  const editRef = useClickOutside(() => {
+    if (getValues("name") !== data.name) {
+      updateWorkspaceName(getValues("name"));
+    } else setIsEditing(false);
+  });
 
-            return res.data;
-        },
-        onSettled:()=>{
-            setIsEditing(false);
+  const { mutate: updateWorkspaceName, isPending } = useMutation({
+    mutationFn: async (newName: string) => {
+      const res = await axios.patch(`/api/workspace/${data.id}`, {
+        name: newName,
+      });
 
-            // revalidatePathname(`/dashboard/${workspace.id}`);
-        }
-    });
+      return res.data;
+    },
+    onSettled: () => {
+      setIsEditing(false);
 
-    useEffect(()=>{
-        console.log(errors);
-        
-        if(errors && Object.keys(errors).length!==0){
-            setShowErrors(true);
-            console.log(errors);
-            
-        }
+      // revalidatePathname(`/dashboard/${workspace.id}`);
+    },
+  });
 
-        setTimeout(()=>{
-            setShowErrors(false);
-        }, 2000);
-    },[errors]);
+  useEffect(() => {
+    console.log(errors);
 
-
-    
-    if(isEditing){
-        
-        return (
-            <form ref={editRef} onSubmit={handleSubmit((data)=>updateWorkspaceName(data.name))}>
-                <Input disabled={isPending} {...register("name")} placeholder={showErrors ? errors.name?.message:""} className={cn(showErrors ? "bg-red-500":null)}/>
-            </form>
-        )
+    if (errors && Object.keys(errors).length !== 0) {
+      setShowErrors(true);
+      console.log(errors);
     }
 
+    setTimeout(() => {
+      setShowErrors(false);
+    }, 2000);
+  }, [errors]);
+
+  if (isEditing) {
     return (
-        <div onClick={()=>setIsEditing(true)} className="font-bold">
-            {workspace.name}
-        </div>
-    )
-}
+      <form
+        ref={editRef}
+        onSubmit={handleSubmit((data) => updateWorkspaceName(data.name))}
+      >
+        <Input
+          disabled={isPending}
+          {...register("name")}
+          placeholder={showErrors ? errors.name?.message : ""}
+          className={cn(showErrors ? "bg-red-500" : null)}
+        />
+      </form>
+    );
+  }
+
+  return (
+    <div onClick={() => setIsEditing(true)} className="font-bold">
+      {data.name}
+    </div>
+  );
+};
