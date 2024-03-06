5ef6aba06fa3b417155d9fed9f24456270231380
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-06 20:52:25 +0200

    chore - visitors countries scroll area

--- app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
+++ app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
@@ -1,10 +1,10 @@
-import ReactCountryFlag from "react-country-flag";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import ReactCountryFlag from "react-country-flag";
 
 export const VisitorsCountries = () => {
   return (
-    <div className="border-r-2 md:border-r-0 border-l-2 md:border-l-0 md:border-b-2 border-purple p-1 md:rounded-br-none h-full">
-      <ScrollArea >
+    <div className="max-h-[400px] border-r-2 md:border-r-0 border-l-2 md:border-l-0 md:border-b-2 border-purple p-1 md:rounded-br-none h-full">
+      <ScrollArea className="h-full">
         <div className="flex items-center gap-2.5 py-2.5">
           <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
@@ -24,60 +24,55 @@ export const VisitorsCountries = () => {
         <div className="flex items-center gap-2.5 py-2.5">
           <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div> <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
           <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div> <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
-        <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
           <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div> <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
-          <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
-          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
-        </div>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>{" "}
         <div className="flex items-center gap-2.5 py-2.5">
           <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
           <span className="text-xs text-darkGray">230 visitors avg.</span>
