798926ed085e8f66c29d14788d3b7ac023ca4d3e
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-06 20:53:23 +0200

    chore - visitors routes scroll area

--- app/dashboard/[id]/components/visitors/VisitedRoutes.tsx
+++ app/dashboard/[id]/components/visitors/VisitedRoutes.tsx
@@ -1,6 +1,9 @@
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 export const VisitedRoutes = () => {
   return (
-    <div className="border-l-2 md:border-b-2 border-r-2 border-purple p-1 md:rounded-bl-sm h-full">
+    <div className="max-h-[400px] border-l-2 md:border-b-2 border-r-2 border-purple p-1 md:rounded-bl-sm h-full">
+      <ScrollArea className="h-full">
       <div className="flex gap-1 cursor-pointer py-2.5">
         <p className="font-bold text-sm">/pricing</p>
         <span className="text-xs text-darkGray self-end">56</span>
@@ -13,6 +16,7 @@ export const VisitedRoutes = () => {
         <p className="font-bold text-sm">/dashboard</p>
         <span className="text-xs text-darkGray self-end">14</span>
       </div>
+      </ScrollArea>
     </div>
   );
 };
