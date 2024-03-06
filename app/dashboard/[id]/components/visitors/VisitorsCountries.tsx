5a47bc422130559bfde5de9708de7d1fe29e8936
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-06 20:46:57 +0200

    chore - scroll-area installed

--- app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
+++ app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
@@ -1,28 +1,88 @@
 import ReactCountryFlag from "react-country-flag";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const VisitorsCountries = () => {
   return (
     <div className="border-r-2 md:border-r-0 border-l-2 md:border-l-0 md:border-b-2 border-purple p-1 md:rounded-br-none h-full">
-      <div className="flex items-center gap-2.5 py-2.5">
-        <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
-        <span className="text-xs text-darkGray">230 visitors avg.</span>
-      </div>
-      <div className="flex items-center gap-2.5 py-2.5">
-        <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
-        <span className="text-xs text-darkGray">230 visitors avg.</span>
-      </div>
-      <div className="flex items-center gap-2.5 py-2.5">
-        <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
-        <span className="text-xs text-darkGray">230 visitors avg.</span>
-      </div>
-      <div className="flex items-center gap-2.5 py-2.5">
-        <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
-        <span className="text-xs text-darkGray">230 visitors avg.</span>
-      </div>
-      <div className="flex items-center gap-2.5 py-2.5">
-        <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
-        <span className="text-xs text-darkGray">230 visitors avg.</span>
-      </div>
+      <ScrollArea >
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div> <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div> <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div> <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"RO"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"US"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"AE"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"BG"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+        <div className="flex items-center gap-2.5 py-2.5">
+          <ReactCountryFlag className="text-2xl" svg countryCode={"FR"} />
+          <span className="text-xs text-darkGray">230 visitors avg.</span>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
