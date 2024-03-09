8398ff50568afba0b6054c70ccd5e92866a0bc70
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-09 20:51:51 +0200

    chore - live user mobile ui

--- app/dashboard/[id]/components/live-users/LiveUser.tsx
+++ app/dashboard/[id]/components/live-users/LiveUser.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import { WorkspaceUser, WorkspaceUserStatus } from "@prisma/client";
 import ReactCountryFlag from "react-country-flag";
 import Image from "next/image";
-import { HiStatusOnline } from "react-icons/hi";
+import { HiDotsHorizontal, HiStatusOnline } from "react-icons/hi";
 import { UserAvatar } from "@/components/UserAvatar";
 import { Dot } from "lucide-react";
 import { GoDotFill } from "react-icons/go";
@@ -19,7 +19,9 @@ export const LiveUser = ({
       <div className="flex items-center gap-2.5">
         <UserAvatar userImage="/blank_user.svg" />
         <div className="">
-          <p className="text-md text-white">{workspaceUser.id} </p>
+          <p className="text-md text-white max-w-[90px] xs:max-w-[150px] xsMd:max-w-[210px] xsBig:max-w-[260px] sm:max-w-full truncate md:max-w-full">
+            {workspaceUser.id}{" "}
+          </p>
           <div className="flex items-center gap-2.5 text-xs">
             <p className="uppercase text-lightGray">online</p>
             <GoDotFill className="text-green-500" />
@@ -27,11 +29,11 @@ export const LiveUser = ({
         </div>
       </div>
 
-      <div className="flex items-center justify-center gap-2.5">
+      <div className="hidden lg:flex items-center justify-center gap-2.5">
         <Image src="/windows.svg" width={25} height={25} alt="browser-icon" />
         <span className="text-sm text-white">{workspaceUser.platform}</span>
       </div>
-      <div className="flex items-center justify-center gap-2.5">
+      <div className="hidden lg:flex items-center justify-center gap-2.5">
         <ReactCountryFlag
           className="text-2xl"
           svg
@@ -40,7 +42,11 @@ export const LiveUser = ({
         <span className="text-sm text-white">{workspaceUser.country}</span>
       </div>
 
-      <div className="flex items-center gap-5 text-lightGray">
+      <div className="hidden xl:flex items-center gap-5 text-lightGray">
+        <div className="flex items-center gap-1">
+          <p className="text-sm">On:</p>
+          <p className="text-xs">/pricing</p>
+        </div>
         <div className="flex items-center gap-1">
           <p className="text-sm">Connected:</p>
           <p className="text-xs">5 minutes ago</p>
@@ -50,6 +56,7 @@ export const LiveUser = ({
           <p className="text-xs">now</p>
         </div>
       </div>
+      <HiDotsHorizontal className="ml-2.5 text-lightGray text-xl cursor-pointer xl:hidden" />
     </div>
   );
 };
