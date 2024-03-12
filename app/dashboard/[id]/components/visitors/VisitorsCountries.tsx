302a634091e19825bf264d1439fb598f2f6ab545
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-12 21:05:33 +0200

    chore - no visitors text

--- app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
+++ app/dashboard/[id]/components/visitors/VisitorsCountries.tsx
@@ -9,25 +9,31 @@ export const VisitorsCountries = ({
 }) => {
   return (
     <div className="max-h-[400px] border-r-2 md:border-r-0 border-l-2 md:border-l-0 md:border-b-2 border-purple p-1 md:rounded-br-none h-full">
-      <ScrollArea className="h-full">
-        {workspaceCountries.map((workspaceCountry) => (
-          <div
-            key={workspaceCountry.countryCode}
-            className="flex items-center gap-2.5 py-2.5"
-          >
-            <ReactCountryFlag
-              className="text-2xl"
-              svg
-              countryCode={workspaceCountry.countryCode}
-            />
-            <p className="text-sm">{workspaceCountry.country}</p>
-            <span className="text-xs text-darkGray ml-auto">
-              {workspaceCountry.visitors}{" "}
-              {workspaceCountry.visitors === 1 ? "visitor" : "visitors"}.
-            </span>
-          </div>
-        ))}
-      </ScrollArea>
+      {workspaceCountries.length > 0 ? (
+        <ScrollArea className="h-full">
+          {workspaceCountries.map((workspaceCountry) => (
+            <div
+              key={workspaceCountry.countryCode}
+              className="flex items-center gap-2.5 py-2.5"
+            >
+              <ReactCountryFlag
+                className="text-2xl"
+                svg
+                countryCode={workspaceCountry.countryCode}
+              />
+              <p className="text-sm">{workspaceCountry.country}</p>
+              <span className="text-xs text-darkGray ml-auto">
+                {workspaceCountry.visitors}{" "}
+                {workspaceCountry.visitors === 1 ? "visitor" : "visitors"}.
+              </span>
+            </div>
+          ))}
+        </ScrollArea>
+      ) : (
+        <p className="text-darkPurple text-xs text-center">
+          No user have visited your app, yet
+        </p>
+      )}
     </div>
   );
 };
