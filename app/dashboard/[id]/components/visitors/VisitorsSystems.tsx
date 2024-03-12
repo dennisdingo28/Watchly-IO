5e7fb8430b316debb634299e253210621f2379a9
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-12 21:04:43 +0200

    chore - no visitors text

--- app/dashboard/[id]/components/visitors/VisitorsSystems.tsx
+++ app/dashboard/[id]/components/visitors/VisitorsSystems.tsx
@@ -10,25 +10,31 @@ export const VisitorsSystems = ({
 }) => {
   return (
     <div className="max-h-[400px] border-l-2 md:border-b-2 border-r-2 md:border-r-0 border-purple p-1 md:rounded-bl-none h-full">
-      <ScrollArea className="h-full">
-        {allWorkspaceSystemOperations.map((systemOperation) => (
-          <div
-            key={systemOperation.systemOperation}
-            className="flex items-center gap-2.5 py-2.5"
-          >
-            <Image
-              src={`/${systemOperation.systemOperation.toLocaleLowerCase()}.svg`}
-              width={25}
-              height={25}
-              alt="browser-icon"
-            />
-            <span className="text-xs text-darkGray ml-auto">
-              {systemOperation.users}{" "}
-              {systemOperation.users === 1 ? "user" : "users"}.
-            </span>
-          </div>
-        ))}
-      </ScrollArea>
+      {allWorkspaceSystemOperations.length > 0 ? (
+        <ScrollArea className="h-full">
+          {allWorkspaceSystemOperations.map((systemOperation) => (
+            <div
+              key={systemOperation.systemOperation}
+              className="flex items-center gap-2.5 py-2.5"
+            >
+              <Image
+                src={`/${systemOperation.systemOperation.toLocaleLowerCase()}.svg`}
+                width={25}
+                height={25}
+                alt="browser-icon"
+              />
+              <span className="text-xs text-darkGray ml-auto">
+                {systemOperation.users}{" "}
+                {systemOperation.users === 1 ? "user" : "users"}.
+              </span>
+            </div>
+          ))}
+        </ScrollArea>
+      ) : (
+        <p className="text-darkPurple text-xs text-center">
+          No user have visited your app, yet
+        </p>
+      )}
     </div>
   );
 };
