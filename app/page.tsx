423477051c4a28a16f81911cf328be7cc4e65191
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-04-02 20:34:05 +0300

    chore - hotjar removed

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ export default function Home({
 }) {
   //
   return (
-    <>
       <main>
         <div className="pt-5">
           <Container>
@@ -65,20 +64,6 @@ export default function Home({
           <Footer />
         </div>
       </main>
-      <Script
-        id="HotJarAnalytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: ` (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3887196,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
-    </>
+      
   );
 }
